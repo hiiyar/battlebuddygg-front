706fe489895873fc75a15a5bb885ddda90e9bc5f
Michele Ambrosio <michele.ambrosio@eduzz.com>
2018-12-02 15:25:30 -0200

    Fix data type on login

--- graphql/login.ts
+++ graphql/login.ts
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export default gql`
-  query loginUser($email: String, $password: String) {
+  query loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       user {
         id
